0146cf372940d11ea38c91d21481b3cd38f5d42c
Fabio Galimberti <fabio.glimb@gmail.com>
2026-04-01 11:02:42 +0200

    chore(g2): bump version and update org links

--- src/lib/app-meta.ts
+++ src/lib/app-meta.ts
@@ -3,9 +3,9 @@ import pkg from '../../package.json';
 export const APP_VERSION = pkg.version;
 
 export const OPENVIDE_LINKS = {
-  github: 'https://github.com/open-vide/openvide',
+  github: 'https://github.com/open-vide/even-open-vide',
   website: 'https://openvide.com',
-  docs: 'https://github.com/open-vide/openvide#readme',
+  docs: 'https://github.com/open-vide/even-open-vide#readme',
 } as const;
 
 export const GUIDE_STORAGE_KEY = 'openvide_guide_dismissed';
